1a98b90c4fd4680bcb6ad2e267183861b3dca054
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2008-04-09 04:45:29 +0000

    setup new service. create blicl project.

---
+++ trunk/new_service/blicl/web/index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb" dir="ltr" >
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="顶呱刮" />
+  <meta name="description" content="顶呱刮" />
+  <meta name="generator" content="Joomla! 1.5 - Open Source Content Management" />
+  <title>Welcome to the Frontpage</title>
+  <link href="/index.php?format=feed&amp;type=rss" rel="alternate" type="application/rss+xml" title="RSS 2.0" />
+  <link href="/index.php?format=feed&amp;type=atom" rel="alternate" type="application/atom+xml" title="Atom 1.0" />
+  <script type="text/javascript" src="/media/system/js/mootools.js"></script>
+  <script type="text/javascript" src="/media/system/js/caption.js"></script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="顶呱刮" />
+  <meta name="description" content="顶呱刮" />
+  <meta name="generator" content="Joomla! 1.5 - Open Source Content Management" />
+  <title>Welcome to the Frontpage</title>
+  <link href="/index.php?format=feed&amp;type=rss" rel="alternate" type="application/rss+xml" title="RSS 2.0" />
+  <link href="/index.php?format=feed&amp;type=atom" rel="alternate" type="application/atom+xml" title="Atom 1.0" />
+  <script type="text/javascript" src="/media/system/js/mootools.js"></script>
+  <script type="text/javascript" src="/media/system/js/caption.js"></script>
+
+<link rel="stylesheet" href="css/template_css.css" type="text/css" />
+<link rel="shortcut icon" href="images/favicon.ico"/>
+
+ <script type="text/javascript">
+function boxes_height()
+{
+  if(document.getElementById('m1') && document.getElementById('m2'))
+  {
+	  if(document.getElementById('m2').clientHeight>document.getElementById('m1').clientHeight)
+	  {
+	    y = document.getElementById('m2').clientHeight;
+	    document.getElementById('m1').style.height = y + 'px';
+	  }
+	  else
+	  {
+	    y = document.getElementById('m1').clientHeight;
+	    document.getElementById('m2').style.height = y + 'px';
+	  }
+	  document.getElementById('m1').style.visibility = 'visible';
+	  document.getElementById('m2').style.visibility = 'visible';
+  }
+
+  else if(document.getElementById('m2'))
+	document.getElementById('m2').style.visibility = 'visible';
+  else if(document.getElementById('m1'))
+	document.getElementById('m1').style.visibility = 'visible';
+}
+</script>
+
+</head>
+
+<body onLoad="boxes_height();">
+
+
+<div id="pagewidth" >
+	<div id="header">
+
+		<div id="mylogo">
+		<img src="images/logo_blicl.jpg" />
+
+		</div>
+
+		<!--
+		<div id="logo"></div>
+		<div id="top2"></div>
+		<div id="top3"></div>
+		<div id="top4"></div>
+		-->
+	</div>
+
+	<div id="wrapper2" class="clearfix" >
+
+		<div id="twocols" class="clearfix">
+
+					<div id="maincol" style="width:575px;">
+
+
+			<div style="clear:both;"><div class="componentheading">
+	Welcome to the Frontpage</div>
+<table class="blog" cellpadding="0" cellspacing="0">
+<tr>
+	<td valign="top">
+					<div>
+
+<table class="contentpaneopen">
+<tr>
+		<td class="contentheading" width="100%">
+					即开型彩票			</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?view=article&amp;catid=34:relatenews&amp;id=47:2008-04-01-18-30-40&amp;format=pdf" title="PDF" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow"><img src="images/pdf_button.png" alt="PDF"  /></a>	</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?view=article&amp;catid=34:relatenews&amp;id=47:2008-04-01-18-30-40&amp;tmpl=component&amp;print=1&amp;page=" title="Print" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;"><img src="/images/M_images/printButton.png" alt="Print"  /></a>	</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?option=com_mailto&amp;tmpl=component&amp;link=aHR0cDovL3d3dy5uc3BhY2UuY24vaW5kZXgucGhwP3ZpZXc9YXJ0aWNsZSZpZD00NzoyMDA4LTA0LTAxLTE4LTMwLTQw" title="E-mail" onclick="window.open(this.href,'win2','width=400,height=300,menubar=yes,resizable=yes'); return false;"><img src="/images/M_images/emailButton.png" alt="E-mail"  /></a>	</td>
+		</tr>
+</table>
+<table class="contentpaneopen">
+
+<tr>
+	<td width="70%"  valign="top" colspan="2">
+		<span class="small">
+			Written by Administrator		</span>
+		&nbsp;&nbsp;
+	</td>
+</tr>
+
+<tr>
+	<td valign="top" colspan="2" class="createdate">
+		Tuesday, 01 April 2008 18:30	</td>
+</tr>
+
+
+<tr>
+<td valign="top" colspan="2">
+直接刮开</td>
+</tr>
+
+
+
+</table>
+<span class="article_separator">&nbsp;</span>
+		</div>
+		</td>
+</tr>
+
+<tr>
+	<td valign="top">
+		<table width="100%"  cellpadding="0" cellspacing="0">
+		<tr>
+										<td valign="top" width="50%" class="article_column">
+
+<table class="contentpaneopen">
+<tr>
+		<td class="contentheading" width="100%">
+					多金彩 乐开怀			</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?view=article&amp;catid=34:relatenews&amp;id=46:m&amp;format=pdf" title="PDF" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow"><img src="images/pdf_button.png" alt="PDF"  /></a>	</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?view=article&amp;catid=34:relatenews&amp;id=46:m&amp;tmpl=component&amp;print=1&amp;page=" title="Print" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;"><img src="/images/M_images/printButton.png" alt="Print"  /></a>	</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?option=com_mailto&amp;tmpl=component&amp;link=aHR0cDovL3d3dy5uc3BhY2UuY24vaW5kZXgucGhwP3ZpZXc9YXJ0aWNsZSZpZD00Njpt" title="E-mail" onclick="window.open(this.href,'win2','width=400,height=300,menubar=yes,resizable=yes'); return false;"><img src="/images/M_images/emailButton.png" alt="E-mail"  /></a>	</td>
+		</tr>
+</table>
+<table class="contentpaneopen">
+
+<tr>
+	<td width="70%"  valign="top" colspan="2">
+		<span class="small">
+			Written by Administrator		</span>
+		&nbsp;&nbsp;
+	</td>
+</tr>
+
+<tr>
+	<td valign="top" colspan="2" class="createdate">
+		Tuesday, 01 April 2008 18:15	</td>
+</tr>
+
+
+<tr>
+<td valign="top" colspan="2">
+<table border="0" cellspacing="8" cellpadding="0" width="600" class="L19 nav2"><tbody><tr align="center"><td width="25%"><a href="http://www.lottery.gov.cn/instant/if_tiy/01.html" target="_blank"><img src="http://images.sports.cn/lottery/images_govlottery/images/instant/tiy01.gif" border="0" alt="激情·梦想_立即体验" width="100" height="100" /></a><br /><strong><font color="#0088d0">激情·梦想</font></strong><br />大奖：25万游戏币<br />每次：10个游戏币<br /><strong>[</strong><a href="http://www.lottery.gov.cn/instant/if_tiy/01.html" target="_blank"><strong>立即体验</strong></a><strong>]</strong></td><td width="25%"><a href="http://www.lottery.gov.cn/instant/if_tiy/02.html" target="_blank"><img src="http://images.sports.cn/lottery/images_govlottery/images/instant/tiy02.gif" border="0" alt="加油！中国队_立即体验" width="100" height="100" /></a><strong><font color="#0088d0"><br />加油！中国队</font></strong><br />大奖：10万游戏币<br />每次： 5个游戏币<br /><strong>[</strong><a href="http://www.lottery.gov.cn/instant/if_tiy/02.html" target="_blank"><strong>立即体验</strong></a><strong>]</strong></td><td width="25%"><a href="http://www.lottery.gov.cn/instant/if_tiy/03.html" target="_blank"><img src="http://images.sports.cn/lottery/images_govlottery/images/instant/tiy03.gif" border="0" alt="奥运金牌_立即体验" width="100" height="100" /></a><br /><strong><font color="#0088d0">奥运金牌</font></strong><br />大奖：10万游戏币<br />每次： 5个游戏币<br /><strong>[</strong><a href="http://www.lottery.gov.cn/instant/if_tiy/03.html" target="_blank"><strong>立即体验</strong></a><strong>]</strong></td><td width="25%"><a href="http://www.lottery.gov.cn/instant/if_tiy/04.html" target="_blank"><img src="http://images.sports.cn/lottery/images_govlottery/images/instant/tiy04.gif" border="0" alt="好运中国_立即体验" width="100" height="100" /></a><br /><strong><font color="#0088d0">好运中国</font></strong><br />大奖： 3万游戏币<br />每次： 3个游戏币<br /><strong>[</strong><a href="http://www.lottery.gov.cn/instant/if_tiy/04.html" target="_blank"><strong>立即体验</strong></a></td></tr></tbody></table></td>
+</tr>
+
+<tr>
+	<td colspan="2"  class="modifydate">
+		Last Updated ( Tuesday, 01 April 2008 18:18 )
+	</td>
+</tr>
+
+
+</table>
+<span class="article_separator">&nbsp;</span>
+
+<table class="contentpaneopen">
+<tr>
+		<td class="contentheading" width="100%">
+					关注奥运主题彩票为中国队加油 访10万大奖得主			</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?view=article&amp;catid=34:relatenews&amp;id=44:award&amp;format=pdf" title="PDF" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow"><img src="images/pdf_button.png" alt="PDF"  /></a>	</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?view=article&amp;catid=34:relatenews&amp;id=44:award&amp;tmpl=component&amp;print=1&amp;page=" title="Print" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;"><img src="/images/M_images/printButton.png" alt="Print"  /></a>	</td>
+
+		<td align="right" width="100%" class="buttonheading">
+	<a href="/index.php?option=com_mailto&amp;tmpl=component&amp;link=aHR0cDovL3d3dy5uc3BhY2UuY24vaW5kZXgucGhwP3ZpZXc9YXJ0aWNsZSZpZD00NDphd2FyZA==" title="E-mail" onclick="window.open(this.href,'win2','width=400,height=300,menubar=yes,resizable=yes'); return false;"><img src="/images/M_images/emailButton.png" alt="E-mail"  /></a>	</td>
+		</tr>
+</table>
+<table class="contentpaneopen">
+
+<tr>
+	<td width="70%"  valign="top" colspan="2">
+		<span class="small">
+			Written by Administrator		</span>
+		&nbsp;&nbsp;
+	</td>
+</tr>
+
+<tr>
+	<td valign="top" colspan="2" class="createdate">
+		Tuesday, 01 April 2008 18:10	</td>
+</tr>
+
+
+<tr>
+<td valign="top" colspan="2">
+<div align="center"><table border="0" cellspacing="0" cellpadding="0" width="550" class="MsoNormalTable" style="width: 412.5pt"><tbody><tr><td style="background-color: transparent; border: #ece9d8; padding: 0cm"><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　在一个普通的周一上午，刚刚从装修工地回家的刘先生经历了他一生中可能是最不平凡的一天，在这天里，他成为了中国奥运主题即开彩票<span>“</span>顶呱刮<span>”</span>的第一位中奖者！<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　朴实的刘先生是山东潍坊人，全家都来烟台工作已经几年了，尽管收入不算丰厚，但也是其乐融融。<span>3</span>月<span>24</span>日，是山东省奥运主题即开彩正式开卖的第一天，在烟台市内很多的销售卖点前都是热闹非凡。刘先生当时正在回家路上，突然看到挂着<span>“</span>奥运主题即开彩<span>”</span>横幅的卖点前人山人海，<span>“</span>起码有几千人啊，我很好奇就过去买，好不容易才买上<span>”</span>刘先生回忆当时购买奥运主题彩票时的情景仍然历历在目。<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　当时刘先生在<span>7</span>种主题票中选择了面值<span>5</span>元的<span>“</span>奥运金牌<span>”</span>玩法，因为刘先生觉得奥运奖牌非常漂亮，其<span>“</span>金镶玉<span>”</span>为设计元素，展现了中华文明与奥林匹克精神的完美结合，同时<span>5</span>元的面值和<span>10</span>万元最高奖的奖金都非常合适。于是他就购买了<span>“</span>奥运金牌<span>”</span>玩法，当他刮开之后，自己还没有看清楚，周围就有人眼尖看到了他中奖了，他自己实在没有想到简单的<span>5</span>元投入，轻松的刮开，连精美的票面还没欣赏够的时候，<span>10</span>万元奖金就已经到手了！<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　刘先生接受采访时始终带着笑声，可见他已经深深的浸入中奖的快乐之中。不过刘先生表示，其实自己当初并没有抱着非中不可的想法，因为他了解体彩的公益性，即便不中奖，这也是为奥运、为社会做了贡献。自己这次中奖之后，还要继续购买奥运主题彩票，因为这个玩法非常简单、轻松，极有意思。<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　当刘先生听说马上还要发行奥运主题的竞猜型彩票之后，他就更加兴奋了，因为他也是一个体育爱好者，是位不折不扣的篮球迷。在所有的奥运项目中，他最喜欢的是篮球比赛，他对记者说，原来中国队曾在世界上拿过第<span>8</span>名的成绩，现在有了姚明和易建联的中国队肯定更加强大，他相信中国队能取得更好的成绩。如果有奥运主题竞猜彩票的话，他不但要在电视机前为中国队加油，还要以购买奥运主题竞猜彩票的形式为奥运加油。<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　对于这次中奖，刘先生笑着告诉记者，他早早的就通知了家人，他的爱人跟他一样高兴，不过尽管他一年的收入也没有这次中奖的奖金多，但他并没有发晕的感觉，因为保持平常心非常重要，自己还要继续上班，继续好好生活，幸福的生活要靠勤劳去创造。而那些奖金，他回去后要和家里人好好商量下用途，如果有余力的话，一定要帮助别的需要帮助的人。<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　在采访的过程中，一口浓重山东口音的刘先生既朴实又爽快，他总在说，中不中奖都是为奥运和公益做贡献。其实每天都有很多人因为体彩中奖而突然感受幸福的喜悦，但这次奥运主题彩票却给彩民们带来了更多更丰富的内涵，因为这不但是中奖的幸福，更有一种和奥运和社会同呼吸共命运的喜悦，是一种参与的幸福，感受的幸福！<span></span></font></span><span style="font-size: 9pt; line-height: 150%; font-family: 宋体"><font color="#000000">　　奥运主题彩票的中奖快乐真的与众不同！<span>(</span>杨天婴<span>)</span></font></span></td></tr></tbody></table></div><span><font face="Times New Roman" size="3" color="#000000"> </font></span></td>
+</tr>
+
+
+
+</table>
+<span class="article_separator">&nbsp;</span>
+				</td>
+						</tr>
+		</table>
+	</td>
+</tr>
+
+</table>
+</div>			</div>
+
+		</div>
+
+			<div id="left">
+				<div id="leftInner">
+						<table cellpadding="0" cellspacing="0" class="moduletable_menu">
+			<tr>
+			<th valign="top">
+				公司简介			</th>
+		</tr>
+			<tr>
+			<td>
+				<ul class="menu"><li id="current" class="active item1"><a href="http://www.nspace.cn/"><span>Home</span></a></li></ul>			</td>
+		</tr>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="moduletable_menu">
+			<tr>
+			<th valign="top">
+				彩票产品			</th>
+		</tr>
+			<tr>
+			<td>
+				<ul class="menu"><li class="item55"><a href="/index.php?option=com_content&amp;view=article&amp;id=46&amp;Itemid=55"><span>多金彩 乐开怀</span></a></li></ul>			</td>
+		</tr>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="moduletable">
+			<tr>
+			<th valign="top">
+				游戏规则			</th>
+		</tr>
+			<tr>
+			<td>
+				<ul class="menu" />			</td>
+		</tr>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="moduletable_menu">
+			<tr>
+			<th valign="top">
+				新闻发布 			</th>
+		</tr>
+			<tr>
+			<td>
+				<ul class="menu"><li class="item53"><a href="/index.php?option=com_content&amp;view=article&amp;id=44&amp;Itemid=53"><span>相关报道（一）</span></a></li><li class="item54"><a href="/index.php?option=com_content&amp;view=article&amp;id=45&amp;Itemid=54"><span>相关报道（二）</span></a></li></ul>			</td>
+		</tr>
+		</table>
+
+				</div>
+			</div>
+	</div>
+
+	<div id="border_bottom"></div>
+	<div id="footer_menu">
+				<div class="moduletable">
+					<ul id="mainlevel-nav"><li><a href="/index.php?option=com_content&amp;view=article&amp;id=25&amp;Itemid=28" class="mainlevel-nav" >About Joomla!</a></li><li><a href="/index.php?option=com_content&amp;view=article&amp;id=22&amp;Itemid=29" class="mainlevel-nav" >Features</a></li><li><a href="/index.php?option=com_newsfeeds&amp;view=newsfeed&amp;id=1&amp;feedid=1&amp;Itemid=18" class="mainlevel-nav" >News</a></li><li><a href="/index.php?option=com_content&amp;view=article&amp;id=27&amp;Itemid=30" class="mainlevel-nav" >The Community</a></li></ul>		</div>
+
+	</div>
+
+	<div id="footer">
+		<div style="float:left;text-align:left; width:436px;">
+			<div align="center">
+	&copy; 2008 顶呱刮彩票</div>
+
+<div align="center">
+	<a href="http://www.joomla.org">Joomla!</a> is Free Software released under the GNU General Public License.</div>		</div>
+		<div style="float:right;padding-top:5px;width:300px;text-align:right;">
+			Design by: <a href="http://www.design-joomla.pl" target="_blank" title="Professional Joomla templates">Joomla and Mambo Templates</a>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
