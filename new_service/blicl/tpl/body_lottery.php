3b93a66e0ac20e001d70dc5b20cc122bb93ddf0e
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2008-04-09 16:42:47 +0000

    images

--- new_service/blicl/tpl/body_lottery.php
+++ new_service/blicl/tpl/body_lottery.php
@@ -45,10 +45,13 @@
 	<table cellpadding="0" cellspacing="0" class="floatLeft" style="width:550px;">
 		<tr>
 			<td valign="top">
-				<img src="images/habao1.jpg" style="width:260px;" />
-			</td>
-			<td valign="top">
-				<img src="images/habao2.jpg" style="width:260px;" />
+				<img src="images/lottery1.jpg" style="width:260px;" />
+				<img src="images/lottery2.jpg" style="width:260px;" />
+				<img src="images/lottery3.jpg" style="width:260px;" />
+				<img src="images/lottery4.jpg" style="width:260px;" />
+				<img src="images/lottery5.jpg" style="width:260px;" />
+				<img src="images/lottery6.jpg" style="width:260px;" />
+				<img src="images/lottery7.jpg" style="width:260px;" />
 			</td>
 		</tr>
 	</table>
