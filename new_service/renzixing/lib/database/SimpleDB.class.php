9dce028ccc18c16b1daf540e013dd04138852563
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2008-04-10 10:16:57 +0000

    SimpleDB

---
+++ new_service/renzixing/lib/database/SimpleDB.class.php
@@ -0,0 +1,75 @@
+<?php
+
+class SimpleDB {
+
+	private static function getConnection() {
+
+		static	$SimpleDBConnectionStatic = null;
+
+		if (empty($SimpleDBConnectionStatic)) {
+
+			// initialize database manager
+			$databaseManager = new sfDatabaseManager();
+			$databaseManager->initialize();
+
+			// batch process here
+
+			$dbInstance = $databaseManager->getDatabase('propel');
+			$SimpleDBConnectionStatic = $dbInstance->getConnection();
+
+		}
+
+		return	$SimpleDBConnectionStatic;
+	}
+
+/*
+	private static function getResource() {
+
+	}
+*/
+
+	public static function execute($SQL) {
+
+		$statement = self::getConnection()->prepareStatement($SQL);
+		$resultset = $statement->executeQuery();
+
+		return	array(
+		#	'resource'	=> $resultset,
+			'bool'		=> $resultset->getResource(),
+			'updated'	=> self::getConnection()->getUpdateCount()
+		);
+	}
+
+
+	public static function fetchAll($SQL) {
+
+		$statement = self::getConnection()->prepareStatement($SQL);
+		$resultset = $statement->executeQuery();
+
+		$arrRet = array();
+		if ($intRecordCount = $resultset->getRecordCount()) {
+			$resultset->first();
+			for ($i = 0; $i < $intRecordCount; ++$i) {
+				$arrRet[]	= $resultset->getRow();
+				$resultset->next();
+			}
+		}
+
+		return	$arrRet;
+
+	}
+
+	public static function escape($string) {
+
+		static	$SimpleDBResourceStatic = null;
+
+		if (empty($SimpleDBResourceStatic)) {
+			$SimpleDBResourceStatic = $connnection->getResource();
+		}
+
+		return	mysql_real_escape_string($string, $SimpleDBResourceStatic);
+
+	}
+
+
+}
