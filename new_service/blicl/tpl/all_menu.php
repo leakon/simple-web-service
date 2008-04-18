7b8df49068ca9b97b69344614a9482bd5446966f
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2008-04-18 16:38:41 +0000

    23

--- new_service/blicl/tpl/all_menu.php
+++ new_service/blicl/tpl/all_menu.php
@@ -13,9 +13,30 @@
 		<tr>
 			<th valign="top"><a href="lottery">彩票相关</a></th>
 		</tr>
+		<?php if ('lottery' == substr(BLUrl::getController(), 0, 7)) : ?>
+		<tr>
+		<td>
+			<ul class="menu">
+				<li class="item1"><a href="lottery_1">玩法介绍</a></li>
+				<li class="item1"><a href="lottery_2">玩法介绍 2</a></li>
+			</ul>
+		</td>
+		</tr>
+		<?php endif ?>
 		<tr>
 			<th valign="top"><a href="recruiting">招聘信息</a></th>
 		</tr>
+
+		<?php if ('recruiting' == substr(BLUrl::getController(), 0, 10)) : ?>
+		<tr>
+		<td>
+			<ul class="menu">
+				<li class="item1"><a href="recruiting_gansu">甘肃省招聘信息</a></li>
+			</ul>
+		</td>
+		</tr>
+		<?php endif ?>
+
 		<tr>
 			<th valign="top"><a href="news">新闻</a></th>
 		</tr>
