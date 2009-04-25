e424f8f67b9d7412622f2a221ef97a1775b756de
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2009-04-25 15:26:31 +0000

    ok

--- new_service/forest/project/data/forest.sql
+++ new_service/forest/project/data/forest.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 07, 2009 at 12:17 AM
+-- Generation Time: Apr 25, 2009 at 11:12 PM
 -- Server version: 5.0.45
 -- PHP Version: 5.2.3
 
@@ -24,16 +24,18 @@ CREATE TABLE `articles` (
   `category_id` int(11) NOT NULL default '0',
   `view_cnt` int(11) NOT NULL default '0',
   `order_num` int(11) NOT NULL default '0',
-  `published` int(11) NOT NULL default '1',
+  `published` int(11) NOT NULL default '0',
+  `is_private` int(11) NOT NULL default '0',
   `created_at` datetime NOT NULL default '1980-01-01 00:00:00',
+  `published_at` datetime NOT NULL default '1980-01-01 00:00:00',
   `title` char(255) NOT NULL,
   `pic` char(255) NOT NULL,
   `keyword` char(255) NOT NULL,
-  `view_group` char(255) NOT NULL,
   `vol_num` char(255) NOT NULL,
   `vol_num_all` char(255) NOT NULL,
   `detail` mediumtext NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  KEY `category` (`category_id`,`published`,`published_at`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -47,5 +49,22 @@ CREATE TABLE `categories` (
   `parent_id` int(11) NOT NULL,
   `order_num` int(11) NOT NULL default '0',
   `name` char(255) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` char(255) NOT NULL,
+  `password` char(32) NOT NULL,
+  `mail` char(255) NOT NULL,
+  `created_at` datetime NOT NULL default '2000-01-01 00:00:00',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
