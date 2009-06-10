70ad0d2d48cad42d3ac69530fad24171cb857d8f
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2009-06-10 08:13:55 +0000

    1234321413241324132

--- trunk/new_service/matcher/project/apps/admin/modules/user/templates/frameFootSuccess.php
+++ trunk/new_service/matcher/project/apps/admin/modules/user/templates/frameFootSuccess.php
@@ -1,12 +1,2 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-</head>
-<body>
+
 <div class="foot">卓傲(北京)贸易有限公司 </div>
-</body>
-</html>
