ac65e96d53e2ed518c99f8b8eadf881241099bdc
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2009-08-10 14:17:52 +0000

    add new final templates

--- trunk/new_service/matcher/project/web/matcher/js/hd_v2.js
+++ trunk/new_service/matcher/project/web/matcher/js/hd_v2.js
@@ -1,9 +1,9 @@
-//滑动菜单效果
+//婊戝姩鑿滃崟鏁堟灉
 function tabit(tabName,btnId,tabNumber){
 	for(i=0;i<tabNumber;i++){
 	document.getElementById(tabName+"_div"+i).style.display = "none";
 	document.getElementById(tabName+"_btn"+i).className = "btu_off";
 }
 	document.getElementById(tabName+"_div"+btnId).style.display = "block";
-	document.getElementById(tabName+"_btn"+btnId).className = "btu_on";	
+	document.getElementById(tabName+"_btn"+btnId).className = "btu_on";
 }
