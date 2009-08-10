89f7ef430742676cd0dea8729d1258aedec28e5e
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2009-08-10 04:23:52 +0000

    update login form height

--- new_service/forest/project/apps/front/templates/layout_simple.php
+++ new_service/forest/project/apps/front/templates/layout_simple.php
@@ -10,7 +10,7 @@ body{font:12px "Lucida Grande", Verdana, Arial, "Bitstream Vera Sans", sans-seri
 #login .topbar{font-size:12px; height:33px; margin:0 0 40px 0; background:url(/images/bg33.png) repeat-x; text-align:left; padding:0 0 0 30px; line-height:33px;}
 #login .topbar a{color:#fff; text-decoration:none; font-weight:normal; }
 #login h2{width:330px; height:61px; background:url(/images/pic330.png) no-repeat; text-indent:-9999px; margin:0 auto 20px auto;}
-#loginform{width:290px; height:170px; background:url(/images/loginBg330x210.png) no-repeat; padding:20px; margin:0 auto;}
+#loginform{width:290px; xheight:170px; background:url(/images/loginBg330x210.png) no-repeat; padding:20px; margin:0 auto;}
 #loginform p{margin-bottom:10px; line-height:25px; text-align:left;}
 #loginform p label{font-size:14px; color:#5cac25;}
 #loginform p label .input{width:270px; height:24px; border:1px solid #ccc; padding:6px 3px 0 3px; }
