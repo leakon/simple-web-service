3c2e45e7f86b44f53a17702c8df69613c715f48b
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-04-08 15:49:21 +0000

    fix bug <6>

--- new_service/matcher/project/apps/front/modules/search/templates/_filterResult.php
+++ new_service/matcher/project/apps/front/modules/search/templates/_filterResult.php
@@ -5,7 +5,10 @@
 
 					if (strlen($dataItem['pic'])) {
 
-						echo	sprintf('<img src="%s" class="list_img" alt="" />', $webUploadDir . $dataItem['pic']);
+						echo	sprintf('<a href="%s" target="_blank"><img src="%s" class="list_img" alt="" /></a>',
+								$dataItem['link'],
+								$webUploadDir . $dataItem['pic']
+							);
 
 					} else {
 
@@ -16,15 +19,34 @@
 				?>
 
 		</li>
-	<li class="li2"><?php echo S::E($dataItem['style']) ?></li>
+	<li class="li2">
+		<?php
+		#	echo S::E($dataItem['style']);
+
+			echo	sprintf('<a href="%s" target="_blank">%s</a>', $dataItem['link'], S::E($dataItem['style']));
+
+		?>
+	</li>
 	<li class="li3">
 		<p><?php echo S::E($dataItem['detail'], 1) ?></p>
+
+		<!--
 		<a href="<?php echo S::E($dataItem['link']) ?>" target="_blank"><?php echo S::E($dataItem['link']) ?></a>
+		-->
+
 	</li>
 	</ul>
 
 		<?php endforeach ?>
 
+
+<?php
+
+#	Debug::pr($arrResult);
+
+
+
+?>
 
 
 <?php if (0) : ?>
--- new_service/matcher/project/apps/front/modules/search/templates/_standResult.php
+++ new_service/matcher/project/apps/front/modules/search/templates/_standResult.php
@@ -8,7 +8,11 @@
 
 					if (strlen($dataItem['pic'])) {
 
-						echo	sprintf('<img src="%s" class="list_img" alt="" />', $webUploadDir . $dataItem['pic']);
+						echo	sprintf('<a href="%s" target="_blank"><img src="%s" class="list_img" alt="" /></a>',
+								$dataItem['link'],
+								$webUploadDir . $dataItem['pic']
+							);
+
 
 					} else {
 
@@ -19,13 +23,29 @@
 				?>
 
 		</li>
-	<li class="li2"><?php echo S::E($dataItem['style']) ?></li>
+	<li class="li2">
+		<?php
+		#	echo S::E($dataItem['style']);
+
+			echo	sprintf('<a href="%s" target="_blank">%s</a>', $dataItem['link'], S::E($dataItem['style']));
+
+		?>
+	</li>
 	<li class="li3">
 
 		<p><?php echo S::E($dataItem['detail'], 1) ?></p>
+
+		<!--
 		<a href="<?php echo S::E($dataItem['link']) ?>" target="_blank"><?php echo S::E($dataItem['link']) ?></a>
+		-->
+
+
 	</li>
 	</ul>
 
 		<?php endforeach ?>
+
+<?php
+
+#	Debug::pr($arrResult);
 
