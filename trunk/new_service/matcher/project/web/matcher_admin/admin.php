deab63e1e6b17d1cb9f037c4dcabb1deea625073
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-08-07 16:08:32 +0000

    程序 ok

--- trunk/new_service/matcher/project/web/matcher_admin/admin.php
+++ trunk/new_service/matcher/project/web/matcher_admin/admin.php
@@ -3,6 +3,6 @@
 
 require_once(dirname(__FILE__).'/../../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('admin', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('admin', 'prod', true);
 
 sfContext::createInstance($configuration)->dispatch();
