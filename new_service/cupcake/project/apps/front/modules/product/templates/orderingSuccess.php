0bcb782ed01b1740f564273b13a23d17fc3d19f1
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-09-20 18:01:00 +0000

    prepare foreach

---
+++ new_service/cupcake/project/apps/front/modules/product/templates/orderingSuccess.php
@@ -0,0 +1,199 @@
+
+
+<form method="post" action="<?php echo url_for('cart/create') ?>">
+
+    <div class="OrderingFolat"><img src="/images/Ordering_top.png" alt="" width="413" height="43" /></div>
+    <div class="OrderingContent">
+      <div class="title"><img src="/images/Ordering_t01.gif" alt="" /><p style="padding-top:5px; margin-bottom:-10px;">Please allow a day notice so we can bake them fresh for you.</p></div>
+
+	  <div style="height:330px; overflow-y:scroll">
+
+      <div class="list">
+        <table width="296" height="110" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img01.jpg" width="53" height="63" alt="" /></td>
+            <td width="197"><h2>Carrot & Zucchini</h2>
+              <p>carro/zucchini/raisins/oats/
+orange/eggs/dairy product/
+cinnamon</p>
+              <h3>￥23/RMB</h3></td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="110" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img02.jpg" width="53" height="63" alt="" /></td>
+            <td width="197"><h2>Mocha</h2>
+              <p>chocolate/coffee/cocoa
+ powder/eggs/dairy product</p>
+              <h3>￥23/RMB</h3></td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="110" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img03.jpg" width="53" height="63" alt="" /></td>
+            <td width="197"><h2>Lemon Curd & Italian Meringue</h2>
+              <p>lemon/eggs/dairy produc/
+gelatine</p>
+              <h3>￥23/RMB</h3></td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="110" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img04.jpg" width="53" height="63" alt="" /></td>
+            <td width="197"><h2>Double Chocolate</h2>
+              <p>chocolate/eggs/dairy product/
+cocoa powder</p>
+              <h3>￥23/RMB</h3></td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="110" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img05.jpg" width="53" height="63" alt="" /></td>
+            <td width="197"><h2>Green Tea & Chocolate Granache</h2>
+              <p>green tea/chocolate/almond/
+eggs/dairy product</p>
+              <h3>￥23/RMB</h3></td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="110" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img06.jpg" width="53" height="63" alt="" /></td>
+            <td width="197"><h2>Orange Poppy seed</h2>
+              <p>orange/poppy seed/eggs/
+dairy product</p>
+              <h3>￥23/RMB</h3></td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+
+
+
+
+
+      <div class="title"><img src="/images/Ordering_t02.png" alt="" width="931" height="40" /></div>
+
+
+
+
+      <div class="list">
+        <table width="296" height="135" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img07.jpg" width="53" height="63" alt="" /></td>
+            <td width="197">
+			<h1>MON.,TUE.</h1>
+			<h2>Chocolate & Banana</h2>
+              <p>chocolate/banana/rum/passion
+fruit/eggs/dairy product/cocoa
+powder</p> <h3>￥23/RMB</h3>
+            </td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="135" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img08.jpg" width="53" height="63" alt="" /></td>
+            <td width="197">
+			<h1>Wed.,Thur.</h1>
+			<h2>Almond & Cherry</h2>
+              <p>almond/cherry/dried berries/
+eggs/dairy product</p><h3>￥23/RMB</h3>
+            </td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="list">
+        <table width="296" height="135" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img09.jpg" width="53" height="63" alt="" /></td>
+            <td width="197">
+			<h1>Fri.,Sat.</h1>
+			<h2>Mango & Passion</h2>
+              <p>fresh mango/passion fruit/
+eggs/dairy product</p><h3>￥23/RMB</h3>
+            </td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+
+	  <div class="list">
+        <table width="296" height="135" border="0" cellspacing="0">
+          <tr>
+            <td width="20" align="center"><input type="checkbox" name="checkbox" value="checkbox" />
+            </td>
+            <td width="57" align="center"><img src="/images/Ordering_img10.jpg" width="53" height="63" alt="" /></td>
+            <td width="197">
+			<h1>Sun.</h1>
+			<h2>Raspberry & Lemon</h2>
+              <p>raspberry/eggs/dairy product/
+gelatine</p><h3>￥23/RMB</h3>
+            </td>
+            <td width="28"><input name="textfield" type="text" value="0" style="width:16px;" />
+            </td>
+          </tr>
+        </table>
+      </div>
+
+	  </div>
+
+
+
+
+
+	  <div class="clear"></div>
+
+      <div class="title" style="padding-top:15px;">
+        <h2>Total:<span>￥220/RMB</span></h2>
+        <a href="Ordering_step2.html"><img src="/images/Ordering_04.gif" alt="" border="0" /></a> </div>
+    </div>
+
+
+
+</form>
+
