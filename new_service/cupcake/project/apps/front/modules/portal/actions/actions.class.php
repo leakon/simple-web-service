cfb5fb362ca30e0e4aa0358499c47dbf203ef366
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-09-20 18:33:38 +0000

    add default portal

---
+++ new_service/cupcake/project/apps/front/modules/portal/actions/actions.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * portal actions.
+ *
+ * @package    cupcake
+ * @subpackage portal
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class portalActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+	public function executeIndex(sfWebRequest $request)
+	{
+
+		$this->setLayout(false);
+
+	}
+}
---
+++ new_service/cupcake/project/apps/front/modules/portal/templates/indexSuccess.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>COLIBRI</title>
+<link href="/css/mainIndex.css" rel="stylesheet" type="text/css" />
+<!--[if IE 6]>
+<script src="/js/PNG.js" language="javascript" type="text/javascript"></script>
+<![ENDif]-->
+
+</head>
+
+<body>
+<div id="main">
+<div id="footer"><span><a href="<?php echo url_for('en/policy') ?>">Privacy Policy</a></span><span><a href="<?php echo url_for('en/findUs') ?>">Find us</a></span><span>&copy;Copyright Colibri.</span> <a href="#" target="_self"><img src="/images/icon01.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon02.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon03.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon04.gif" alt="" border="0" align="absmiddle" /></a></div>
+<div class="inlogo">
+<a href="<?php echo url_for('en/index') ?>" target=""><img src="/images/index_logo.png" alt="" border="0" /></a><br />
+<!--<a href="#" target="_blank"><img src="/images/bot_ch.gif" alt="" border="0" /></a>--><a href="<?php echo url_for('en/index') ?>" target=""><img src="/images/bot_en.gif" alt="" border="0" /></a></div>
+
+
+</div>
+</body>
+</html>
