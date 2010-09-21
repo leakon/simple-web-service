da9ad94e97217a5195a2c30d26204a712e7d8e15
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-09-21 16:41:04 +0000

    add highlight

--- trunk/new_service/cupcake/project/apps/front/templates/layout.php
+++ trunk/new_service/cupcake/project/apps/front/templates/layout.php
@@ -41,6 +41,60 @@ function SubmitOrderForm(strFormID, CheckFormFunc) {
 <body>
 <?php
 
+$arrNav		= array(
+			0	=> array('COLIBRI', url_for('en/index'), array(
+										'en/index'	=> 1,
+										)
+					),
+			
+			1	=> array('MENU', url_for('en/menu'), array(
+										'en/menu'	=> 1,
+										'en/cupcakes'	=> 1,
+										'en/panini'	=> 1,
+										)
+					),
+					
+			2	=> array('ORDER', url_for('product/index'), array(
+										'product/index'		=> 1,
+										'cart/fillAddress'	=> 1,
+										'cart/selectPayment'	=> 1,
+										)
+					),
+					
+			3	=> array('FAQ', url_for('en/faq'), array(
+										'en/faq'	=> 1,
+										)
+					),
+					
+		);
+
+$module		= $sf_context->getModuleName();
+$action		= $sf_context->getActionName();
+
+function showNav($arrNav, $module, $action) {
+	
+	$route		= $module . '/' . $action;
+	
+	$arrHtmls	= array();
+	
+	foreach ($arrNav as $val) {
+		
+		if (isset($val[2][$route])) {
+			// highlight
+			
+			$arrHtmls[]	= $val[0];
+			
+		} else {
+			
+			$arrHtmls[]	= sprintf('<a href="%s">%s</a>', $val[1], $val[0]);
+		}
+		
+	}
+	
+	return	implode('<span>/</span>', $arrHtmls);
+	
+}
+
 ?>
 <div id="main">
 <div id="footer"><span><a href="<?php echo url_for('en/policy') ?>">Privacy Policy</a></span><span><a href="<?php echo url_for('en/findUs') ?>">Find us</a></span><span>&copy;Copyright Colibri.</span> <a href="#" target="_self"><img src="/images/icon01.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon02.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon03.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon04.gif" alt="" border="0" align="absmiddle" /></a></div>
@@ -48,7 +102,7 @@ function SubmitOrderForm(strFormID, CheckFormFunc) {
   <div class="logo"><a href="<?php echo url_for('en/index') ?>"><img src="/images/logo.png" alt="" border="0" /></a></div>
 
   <div class="nav"><img src="/images/bot03.gif" alt="" /><img src="/images/bot04.gif" alt="" /><br />
-    COLIBRI<span>/</span><a href="<?php echo url_for('en/menu') ?>">MENU</a><span>/</span><a href="<?php echo url_for('product/index') ?>">ORDER</a><span>/</span><a href="<?php echo url_for('en/faq') ?>">FAQ</a></div>
+    <?php echo showNav($arrNav, $module, $action) ?></div>
 	</div>
 	<div class="clear"></div>
 
