f6f2d31a09f3d6ed8254c0b19eff1c41f6580627
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-09-21 10:39:35 +0000

    add admin module

---
+++ new_service/cupcake/project/apps/front/modules/admin/templates/listOrderSuccess.php
@@ -0,0 +1,69 @@
+
+
+
+<table class="product_list" border="1" style="width:900px;">
+
+<tr>
+	<td width="32">订单</td>
+	<td width="">总价</td>
+	<td width="">付款方式</td>
+	<td width="">状态</td>
+	<td width="">客户姓名</td>
+	<td width="">客户手机</td>
+	<td width="">客户地址</td>
+	<td width="">送货时间</td>
+	<td width="">创建时间</td>
+</tr>
+
+<?php
+
+$arrMethodMap	= array(
+
+	Table_data_order::PAY_METHOD_CASH	=> '现金',		// 
+	Table_data_order::PAY_METHOD_ALIPAY	=> '支付宝',		// 
+	Table_data_order::PAY_METHOD_PAYPAL	=> 'Paypal',		// 
+
+);
+
+
+$arrStatusMap	= array(
+
+	0					=> '未付款',		// 
+	Table_data_order::STATUS_PAYED_SUCCESS	=> '已付款',		// 
+
+);
+
+foreach ($arrResult as $key => $item) :
+	
+	
+?>
+
+
+<tr>
+	<td><?php echo $item['order_id'] ?></td>
+	<td><?php echo $item['total'] ?></td>
+	<td>
+		<?php echo $arrMethodMap[  $item['pay_method'] ] ?>
+	</td>
+	<td>
+		<?php echo $arrStatusMap[ $item['status'] ] ?>
+	</td>
+	<td><?php echo $item['customer_name'] ?>&nbsp;</td>
+	<td><?php echo $item['customer_mobile'] ?></td>
+	<td><?php echo $item['customer_address'] ?></td>
+	<td><?php echo $item['receive_time'] ?></td>
+	<td><?php echo $item['created_at'] ?></td>
+</tr>
+
+
+
+<?php
+
+endforeach;
+
+?>
+
+</table>
+
+
+
