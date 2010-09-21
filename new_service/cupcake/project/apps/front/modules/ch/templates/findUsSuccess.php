11935c534732276ab337e89c3f7016e2b27fb06f
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-09-21 15:29:33 +0000

    add ch module

---
+++ new_service/cupcake/project/apps/front/modules/ch/templates/findUsSuccess.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+中文
+
+*/
+
+?>
+
+	<div class="aboutFolat"><img src="/images/About_02.png" alt="" /></div>
+	<div class="aboutContentLeft"><img src="/images/About_01.jpg" alt="" /></div>
+	<div class="aboutContentRight">
+	
+	<p><strong>地址：</strong><br />
+	  北京市朝阳区三里屯路11号院地下一层NLG-51单元<br />
+10027</p>
+	  <p><strong>电话：</strong><br />
+	 +8610 6417 0808<br />
++8610 6417 7799</p>
+	  <p><strong>邮箱：</strong><br />
+	  info@colibricupcakes.com</p>
+	<p><strong>营业时间：</strong><br />
+	  上午10：00－晚上10：00</p>
+	</div>
