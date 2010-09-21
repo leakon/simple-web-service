690868ae1ca2587a8bfa4ae1e5b5bdf16f2f6adc
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-09-21 16:41:04 +0000

    add highlight

--- new_service/cupcake/project/apps/front/templates/layout_ch.php
+++ new_service/cupcake/project/apps/front/templates/layout_ch.php
@@ -41,6 +41,60 @@ function SubmitOrderForm(strFormID, CheckFormFunc) {
 <body>
 <?php
 
+$arrNav		= array(
+			0	=> array('蜂鸟', url_for('ch/index'), array(
+										'ch/index'	=> 1,
+										)
+					),
+			
+			1	=> array('菜单', url_for('ch/menu'), array(
+										'ch/menu'	=> 1,
+										'ch/cupcakes'	=> 1,
+										'ch/panini'	=> 1,
+										)
+					),
+					
+			2	=> array('订餐', url_for('product/indexCh'), array(
+										'product/indexCh'		=> 1,
+										'cart/fillAddress'	=> 1,
+										'cart/selectPayment'	=> 1,
+										)
+					),
+					
+			3	=> array('问答', url_for('ch/faq'), array(
+										'ch/faq'	=> 1,
+										)
+					),
+					
+		);
+
+$module		= $sf_context->getModuleName();
+$action		= $sf_context->getActionName();
+
+function showNav($arrNav, $module, $action) {
+	
+	$route		= $module . '/' . $action;
+	
+	$arrHtmls	= array();
+	
+	foreach ($arrNav as $val) {
+		
+		if (isset($val[2][$route])) {
+			// highlight
+			
+			$arrHtmls[]	= $val[0];
+			
+		} else {
+			
+			$arrHtmls[]	= sprintf('<a href="%s">%s</a>', $val[1], $val[0]);
+		}
+		
+	}
+	
+	return	implode('<span>/</span>', $arrHtmls);
+	
+}
+
 ?>
 <div id="main">
 <div id="footer"><span><a href="<?php echo url_for('ch/policy') ?>">隐私条款</a></span><span><a href="<?php echo url_for('ch/findUs') ?>">联系我们</a></span><span>&copy;Copyright Colibri.</span> <a href="#" target="_self"><img src="/images/icon01.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon02.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon03.gif" alt="" border="0" align="absmiddle" /></a><a href="#" target="_self"><img src="/images/icon04.gif" alt="" border="0" align="absmiddle" /></a></div>
@@ -48,7 +102,7 @@ function SubmitOrderForm(strFormID, CheckFormFunc) {
   <div class="logo"><a href="<?php echo url_for('ch/index') ?>"><img src="/images/logo.png" alt="" border="0" /></a></div>
 
   <div class="nav"><img src="/images/bot03.gif" alt="" /><img src="/images/bot04.gif" alt="" /><br />
-    蜂鸟<span>/</span><a href="<?php echo url_for('ch/menu') ?>">菜单</a><span>/</span><a href="<?php echo url_for('product/indexCh') ?>">订餐</a><span>/</span><a href="<?php echo url_for('ch/faq') ?>">问答</a></div>
+    <?php echo showNav($arrNav, $module, $action) ?></div>
 	</div>
 	<div class="clear"></div>
 
