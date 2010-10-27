51ae2d418b46de768cfb8b394d5424df61916df2
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-10-27 16:15:07 +0000

    修改邮件提示

--- new_service/cupcake/project/apps/front/modules/admin/actions/actions.class.php
+++ new_service/cupcake/project/apps/front/modules/admin/actions/actions.class.php
@@ -10,107 +10,111 @@
  */
 class adminActions extends sfActions
 {
-  	
-  	
+
+
 	public function executeOrderDetailPass(sfWebRequest $request) {
-		
+
 		$strPass		= $request->getParameter('pass');
-		
+
 		if ('cupcakes' != $strPass) {
 			die('None');
 		}
-		
+
 		$this->setLayout('layout_pass');
 		$this->setTemplate('orderDetail');
-		
+
 		$strOrderID		= $request->getParameter('order_id');
-		
+
 		$this->getDetail($strOrderID);
-		
+
 		// 留空，避免用户支付宝付款后，还提示未付款
-		$this->arrOrderDetail['status']		= 'empty';
-		
-		
+		// 仅当支付宝付款，且付款状态为【未付款】时，才设置 empty
+		if ($this->arrOrderDetail['pay_method'] == Table_data_order::PAY_METHOD_ALIPAY
+			&& $this->arrOrderDetail['status'] == 0) {
+			$this->arrOrderDetail['status']		= 'empty';
+		}
+
+
 	}
-	
+
 	protected function getDetail($strOrderID) {
-		
+
 		$this->arrOrderDetail	= Table_data_order::getDetail($strOrderID);
-		
+
 		if (empty($this->arrOrderDetail['order_id'])) {
 			$this->renderText('Order not found');
 		}
-		
-		
+
+
 		// 获取客户信息
 		$objCustomer		= new Table_data_customer();
 		$objCustomer->order_id	= $this->arrOrderDetail['order_id'];
-		
+
 		$arrCustomer		= SofavDB_Record::match($objCustomer, false);
-			
+
 		$this->arrOrderDetail['customer_name']		= $arrCustomer['name'];
 		$this->arrOrderDetail['customer_mobile']	= $arrCustomer['mobile'];
 		$this->arrOrderDetail['customer_address']	= $arrCustomer['address'];
 		$this->arrOrderDetail['receive_time']		= $arrCustomer['receive_time'];
-			
+
 	}
-	
-  	
+
+
 	public function executeOrderDetail(sfWebRequest $request) {
-		
+
 		$this->setLayout('admin');
-		
+
 		$strOrderID		= $request->getParameter('order_id');
-		
+
 		$this->getDetail($strOrderID);
-		
+
 	#	Debug::pre($this->arrOrderDetail);
-				
+
 	#	return	sfView::NONE;
-		
+
 	}
-  
+
 
 	public function executeListOrder(sfWebRequest $request)  {
-		
+
 		$this->setLayout('admin');
-		
+
 		$criteria			= new SofavDB_Criteria(' ORDER BY created_at DESC ');
 		$this->arrOrderResult		= SofavDB_Record_SE::findAll('Table_data_order', $criteria, false);
 		$this->arrOrderResult		= Array_Util::toKeyIndexed($this->arrOrderResult, 'order_id');
-		
+
 		$criteria			= new SofavDB_Criteria('  ');
 		$this->arrProdResult		= SofavDB_Record_SE::findAll('Table_data_product', $criteria, false);
 		$this->arrProdResult		= Array_Util::toKeyIndexed($this->arrProdResult, 'id');
-		
+
 		$criteria			= new SofavDB_Criteria('  ');
 		$this->arrCustomerResult	= SofavDB_Record_SE::findAll('Table_data_customer', $criteria, false);
 		$this->arrCustomerResult	= Array_Util::toKeyIndexed($this->arrCustomerResult, 'order_id');
-		
+
 	#	Debug::pr($this->arrOrderResult);
 	#	Debug::pr($this->arrProdResult);
 	#	Debug::pr($this->arrCustomerResult);
-		
-		
+
+
 		$this->arrResult	= array();
-		
+
 		foreach ($this->arrOrderResult as $key => $val) {
-			
-			
+
+
 			$val['customer_name']		= $this->arrCustomerResult[$key]['name'];
 			$val['customer_mobile']		= $this->arrCustomerResult[$key]['mobile'];
 			$val['customer_address']	= $this->arrCustomerResult[$key]['address'];
 			$val['receive_time']		= $this->arrCustomerResult[$key]['receive_time'];
-			
-			
+
+
 			$this->arrResult[$key]	= $val;
-			
+
 		}
-		
-		
-		
+
+
+
 	#	Debug::pr($this->arrResult);
-		
+
 	}
 
 
@@ -129,16 +133,16 @@ class adminActions extends sfActions
 	}
 
 	public function executeIndex(sfWebRequest $request) {
-		
+
 		if ($this->intUserId > 0) {
-			
+
 			return	$this->redirect('admin/listOrder');
-			
+
 		}
-		
+
 		$this->forward('admin', 'signIn');
-		
-		
+
+
 	}
 
 	public function executeSignIn(sfWebRequest $request) {
--- new_service/cupcake/project/lib/SofavDB_Model/sf_table_data_order.class.php
+++ new_service/cupcake/project/lib/SofavDB_Model/sf_table_data_order.class.php
@@ -6,14 +6,14 @@
  */
 
 class Table_data_order extends SofavDB_Table {
-	
+
 	const
 		PAY_METHOD_CASH		= 1000,		// 现金
 		PAY_METHOD_ALIPAY	= 2000,		// 支付宝
 		PAY_METHOD_PAYPAL	= 3000,		// Paypal
-		
+
 		STATUS_PAYED_SUCCESS	= 2000,		// 付款成功
-		
+
 		VERSION			= 0;
 
 	public function initialize() {
@@ -32,138 +32,139 @@ class Table_data_order extends SofavDB_Table {
 		$this->hasColumns($arrColumns);
 
 	}
-	
+
 	public static function getDiscount($intTotal) {
-		
+
 		if ($intTotal >= 220) {
-			
+
 			$intDivide	= floor($intTotal / 220);
-			
+
 			$intTotal	-= $intDivide * 56;
-			
+
 		} else if ($intTotal >= 138) {
-			
+
 			$intTotal	-= 23;
-			
+
 		}
-		
+
 		return	$intTotal;
-		
+
 	}
-		
-	
+
+
 	public static function getDetail($strOrderID) {
-			
+
 		$arrReturn		= array();
-		
+
 		// 获取订单信息
 		$objOrder		= new Table_data_order();
 		$objOrder->order_id	= $strOrderID;
-		
+
 		$arrOrder		= SofavDB_Record::match($objOrder, false);
-		
+
 	#	Debug::pr($arrOrder);
-		
+
 		if (isset($arrOrder['id'])) {
-			
+
 			$arrMapCategory		= array(
-							
+
 							Table_data_product::CATEGORY_NORMAL	=> 'normal',
 							Table_data_product::CATEGORY_SPECIAL	=> 'special',
-						
+
 						);
-			
+
 			// 查询订单详情
-				
-		
-		
+
+
+
 			$criteria	= new SofavDB_Criteria(sprintf('WHERE @where'));
 			$arrParam	= array(
 						'order_id'	=> $strOrderID,
 					);
-					
+
 			$arrResult	= SofavDB_Record_SE::findAll('Table_data_order_detail', $criteria->bind($arrParam), false);
-			
+
 			$arrProducts	= Table_data_product::getAllProducts();
-			
-			
+
+
 			$arrDetail	= array();
-			
+
 			foreach ($arrResult as $val) {
-				
+
 				$intProductID	= $val['product_id'];
-				
+
 				$intCategory	= $arrProducts[$intProductID]['category'];
-				
+
 				$arrOne		= array(
-				
+
 							'product_name'		=> $arrProducts[$intProductID]['name'],
 							'category'		=> $arrMapCategory[$intCategory],
 							'pic'			=> $arrProducts[$intProductID]['pic'],
 							'price'			=> $arrProducts[$intProductID]['price'],
 							'quantity'		=> $val['quantity'],
-						
+
 						);
-						
+
 				$arrDetail[]	= $arrOne;
-				
+
 			}
-			
+
 		#	Debug::pr($arrResult);
 		#	Debug::pr($arrProducts);
 		#	Debug::pr($arrDetail);
-			
+
 			$arrReturn		= $arrOrder;
-			
+
 			$arrReturn['detail']	= $arrDetail;
-		
+
 		}
-		
+
 	#	Debug::pr($arrReturn);
-		
+
 		return	$arrReturn;
-		
+
 	}
-	
-	
+
+
 	public static function getOrderHtml($strOrderID) {
-		
+
 		// 获取邮件正文
-		
+
 		// SYMFONY_SERVER_HOST
 		$strHost	= SYMFONY_SERVER_HOST;
-		
+
 	//	if (1 || file_exists('F:/usr/ColibriCupcakes/')) {
 		if (false !== strpos(SYMFONY_SERVER_HOST, 'www.colibricupcakes.com')) {
 			$strHost	= 'colibricupcakes.baolaa.com';
 		}
-		
+
 		$strUrl		= sprintf('http://%s/index.php/admin/orderDetailPass/order_id/%s?pass=cupcakes',
 						$strHost, $strOrderID);
-						
+
 		$strHtml	= file_get_contents($strUrl);
-		
-		$log		= sprintf("order_id[%s]	url[%s]", 
+
+		$log		= sprintf("order_id[%s]	url[%s]",
 					$strOrderID, $strUrl);
 		MyLog::doLog($log);
-		
+
 		/*
 		*/
 		return	$strHtml;
-		
+
 	}
-	
+
 	public static function sendOrderDetailMail($strOrderID) {
-		
+
 		$strHtml	= self::getOrderHtml($strOrderID);
-		
+
 		$address	= array(
 					'leakon@hotmail.com',
-				//	'info@colibricupcakes.com',
+					'info@colibricupcakes.com',
 				);
-		
-		$res		= MailWork::send($address, 'COLIBRI Cup Cakes Order Info - ' . $strOrderID, $strHtml);
-	
+
+		$title		= sprintf('Order [%s] COLIBRI Cup Cakes', $strOrderID);
+		$res		= MailWork::send($address, $title, $strHtml);
+
 	}
 
 }
