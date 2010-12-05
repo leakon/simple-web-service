0f828969379d4163a19e03bd9f3d5113b1480ac9
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-12-05 15:29:13 +0000

    Move delivery date and time

--- new_service/cupcake/project/apps/front/modules/product/templates/orderingSuccess.php
+++ new_service/cupcake/project/apps/front/modules/product/templates/orderingSuccess.php
@@ -1,6 +1,6 @@
 
 <script>
-	
+
 </script>
 
 <form method="post" id="id_order_form" action="<?php echo url_for('cart/create') ?>">
@@ -8,6 +8,44 @@
     <div class="OrderingFolat"><img src="/images/Ordering_top.png" alt="" width="413" height="43" /></div>
     <div class="OrderingContent">
       <div class="title"><img src="/images/Ordering_t01.gif" alt="" /><p style="padding-top:5px; margin-bottom:-10px;">Please allow a day notice so we can bake them fresh for you.</p></div>
+
+	<table width="100%" border="0" cellspacing="0" cellpadding="6">
+	 <tr>
+    <td width="12%" align="right">Delivery Day</td>
+    <td width="88%">
+
+		<?php
+
+			$arrConf	= array(
+
+						'rich'		=> true,
+						'size'		=> '',
+						'calendar_button_img'	=> '/images/date_control.gif'
+
+					);
+
+			echo	input_date_tag('receive_day', date("Y-m-d", time() + 86400), $arrConf);
+
+		?>
+
+
+
+        </td>
+  </tr>
+   <tr>
+    <td width="12%" align="right">Delivery Time</td>
+    <td width="88%"><label>
+        <select name="receive_time" size="1">
+
+        	<?php
+        		echo	Table_data_cart::genDeliverTimes();
+        	?>
+
+        </select>
+        </label></td>
+  </tr>
+</table>
+
 
 	  <div style="height:330px; overflow-y:scroll">
 
