c906a00ba249211b01d4f933c9f23fa5faedac00
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2010-12-21 10:14:10 +0000

    add script to layout

--- new_service/cupcake/project/apps/front/templates/layout.php
+++ new_service/cupcake/project/apps/front/templates/layout.php
@@ -135,6 +135,14 @@ function showNav($arrNav, $module, $action) {
 
 </script>
 
+<script type="text/javascript">
+
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F99c00aec131e01b1ae9b6e21d33f388c' type='text/javascript'%3E%3C/script%3E"));
+
+</script>
+
 </body>
 </html>
 
