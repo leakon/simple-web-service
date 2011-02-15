a214fcfa31690d7e7651212e729ef556ca196faa
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2011-02-15 15:30:04 +0000

    update to new menu

--- trunk/new_service/cupcake/project/apps/front/modules/en/templates/paniniSuccess.php
+++ trunk/new_service/cupcake/project/apps/front/modules/en/templates/paniniSuccess.php
@@ -22,43 +22,7 @@
 	<div class="menuContentRight">
 	<div style="height:330px; overflow-y:scroll">
 		
-		
-	<p><strong>Panini</strong><br />
-	  Tomato chutney, Fresh basil, Cheese <span>￥38</span><br />
-	Salami, Roasted red pepper, Radicchio, Cheese <span>￥48</span><br />
-	Creamy chicken, Sauteed mushroom, Cheese <span>￥48</span></p>
-
-
-	<p><strong>Sourdough</strong><br />
-	  Bacon, Fried quail egg, Radicchio, Mayonnaise <span>￥38</span><br />
-	  Egg Mayo <span>￥38</span><br />
-	  Poached salmon, Mint & dill yoghurt dressing, Cucumber, Salmon roe <span>￥58</span></p>
-
-
-	  <p><strong>Wrap</strong><br />
-      Sambal prawn, Rice crispy, Cress, Thai dressing <span>￥48</span><br />
-      Antipasto <span>￥48</span><br />
-      Peking duck, Hoi sin sauce, Pesto, Cucumber, Spring onion, Prawn cracker, Buttercup lettuce <span>￥48</span></p>
-
-
-	<p><strong>Baguette</strong><br />
-	  Ham, Pickled pineapple, Coriander, Mint leaves,Lime mayonnaise, Buttercup lettuce <span>￥38</span><br />Roasted beef, Gherkin, Horseradish, Rocket leaves <span>￥48</span><br />
-	  Smoked chicken, Avocado, Crab meat, Tomatoes, Lime mayonnaise, Romaine lettuce <span>￥48</span></p>
-
-
-	  <p><strong>Salads</strong><br />
-      Tuna Nicoise <span>￥48</span><br />
-      Couscous salad, Roasted pumpkin <span>￥48</span><br />
-      Roasted cauliflower, Radicchio, Feta, Bacon, Crouton, Quail egg, Red grapes, Red wine dressing <span>￥48</span></p>
-
-
-
-
-	  <p><strong>Soup</strong><br />
-	  Pumpkin Soup <span>￥28</span><br />
-	  Chicken and Rice Soup <span>￥28</span></p>
-
-
+	
 
 	<p><strong>PANINI</strong>
 		<br />Tomato chutney, Fresh basil, Cheese <span>￥38.00/RMB</span>
