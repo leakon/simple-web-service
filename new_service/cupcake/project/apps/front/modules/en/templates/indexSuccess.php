f2e5daeb1e0461d882142835df6db07bb517c2ac
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2011-02-15 15:19:57 +0000

    text

--- new_service/cupcake/project/apps/front/modules/en/templates/indexSuccess.php
+++ new_service/cupcake/project/apps/front/modules/en/templates/indexSuccess.php
@@ -12,5 +12,5 @@
 	<div class="aboutContentRight">
 	<p><img src="/images/About_03.gif" alt="" /></p>
 	<p>"Each cake is made from scratch and baked fresh every day. We source the highest quality ingredients including free range eggs, creamy butter, vanilla and Valrhona chocolate. Each cake is hand finished and topped with a splash of colour. "Expect adventurous flavours that 
-are stylishly presented. The cupcakes are a cheeky indulgence perfect for a little celebratory treat." Says Kim the Malaysian pastry chef who is trained at Le Cordon Bleu Paris and worked in pastry kitchens in Paris, London, Sydney and now Beijing.</p>
+are stylishly presented. The cupcakes are a cheeky indulgence perfect for a little celebratory treat." Le Cordon Bleu Paris and worked in Beijing.</p>
 </div>
