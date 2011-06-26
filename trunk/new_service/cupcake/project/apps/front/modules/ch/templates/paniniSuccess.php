6ef96204148f30001a6f77b41bb05baac642ca88
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2011-06-26 03:59:34 +0000

    update menu

--- trunk/new_service/cupcake/project/apps/front/modules/ch/templates/paniniSuccess.php
+++ trunk/new_service/cupcake/project/apps/front/modules/ch/templates/paniniSuccess.php
@@ -23,47 +23,63 @@
 	<div style="height:330px; overflow-y:scroll">
 	
 	<p><strong>帕尼尼</strong>
-		<br />秘制番茄酱, 鲜罗勒, 大孔奶酪 <span>￥38.00/RMB</span>
-		<br />意大利腊肠, 烤红柿子椒, 紫菊苣, 大孔奶酪 <span>￥48.00/RMB</span>
-		<br />烟熏鸡胸肉, 炒蘑菇, 大孔奶酪 <span>￥48.00/RMB</span>
+		<br />秘制番茄酱，鲜罗勒，大孔奶酪 <span>￥38.00/RMB</span>
+		<br />意大利腊肠，烤红柿子椒，紫菊苣，大孔奶酪 <span>￥48.00/RMB</span>
+		<br />烟熏鸡胸肉，炒蘑菇，大孔奶酪 <span>￥48.00/RMB</span>
+		<br />炒牛肉，红柿子椒，洋葱，大孔奶酪 <span>￥48.00/RMB</span>
 	</p>
 	  
 	
 	<p><strong>百吉饼</strong>
-		<br />烤培根, 鸡蛋蛋黄酱, 西红柿, 黄油生菜 <span>￥38.00/RMB</span>
+		<br />烤培根，鸡蛋蛋黄酱，西红柿，黄油生菜 <span>￥38.00/RMB</span>
 		<br />火腿，菠萝，秘制青椒，罗马生菜，青柠蛋黄酱 <span>￥48.00/RMB</span>
-		<br />烟熏三文鱼,柠檬奶油乳酪，香葱，黄瓜, 芝麻菜 <span>￥58.00/RMB</span>
+		<br />烟熏三文鱼，柠檬奶油乳酪，香葱，黄瓜，芝麻菜 <span>￥58.00/RMB</span>
+		<br />烤鸡腿肉，西红柿，奶油生菜 <span>￥48.00/RMB</span>
+		<br />烤鱼肉，泰式蛋黄酱，黄瓜，西红柿，芝麻菜 <span>￥58.00/RMB</span>
 	</p>
 	
 	
 	<p><strong>卷饼</strong>
-		<br />三巴虾, 卜卜米, 西洋菜, 泰式汁 <span>￥48.00/RMB</span>
-		<br />烤鸭, 海鲜酱, 罗勒酱, 黄瓜, 香葱, 虾片, 黄油生菜 <span>￥48.00/RMB</span>
+		<br />三巴虾，卜卜米，西洋菜，泰式汁 <span>￥48.00/RMB</span>
+		<br />烤鸭，海鲜酱，罗勒酱，黄瓜，香葱，虾片，黄油生菜 <span>￥48.00/RMB</span>
+		<br />金枪鱼沙拉，大孔奶酪，凯撒酱 <span>￥48.00/RMB</span>
 	</p>
 	
 	
 	<p><strong>法棍面包</strong>
-		<br />烤猪肉, 秘制紫甘蓝, 混合沙拉 <span>￥48.00/RMB</span>
-		<br />香烤牛肉, 酸黄瓜, 辣根酱, 芝麻生菜 <span>￥48.00/RMB</span>
-		<br />烟熏鸡胸肉, 鳄梨果, 蟹肉, 鲜番茄,  青柠蛋黄酱, 罗马生菜 <span>￥48.00/RMB</span>
+		<br />香烤牛肉，酸黄瓜，辣根酱，芝麻菜 <span>￥48.00/RMB</span>
+		<br />烟熏鸡胸肉，鳄梨果，蟹肉，鲜番茄，青柠蛋黄酱，罗马生菜 <span>￥48.00/RMB</span>
+		<br />意大利香肠，秘制紫椰菜，番茄酱 <span>￥48.00/RMB</span>
+		<br />水牛芝士，紫苏酱，鲜番茄，奶油生菜 <span>￥48.00/RMB</span>
 	</p>
 	
 	
 	<p><strong>沙拉</strong>
 		<br />金枪鱼沙拉 <span>￥48.00/RMB</span>
-		<br />烤菜花, 烤紫菊苣, 飞达芝士, 培根, 烤面包丁,  鹌鹑蛋, 红酒醋汁 <span>￥48.00/RMB</span>
+		<br />烤菜花，烤紫菊苣，飞达芝士，培根，烤面包丁，鹌鹑蛋，红酒醋汁 <span>￥48.00/RMB</span>
+		<br />意大利面沙拉 <span>￥48.00/RMB</span>
 	</p>
 	
 	<p><strong>汤</strong>
 		<br />南瓜汤 <span>￥28.00/RMB</span>
-		<br />大葱土豆汤 <span>￥28.00/RMB</span>
+		<br />芦笋汤 <span>￥28.00/RMB</span>
 	</p>
 
 	<p><strong>其他</strong>
-		<br />烤土豆，配菜心沙拉，金枪鱼 <span>￥48.00/RMB</span>
-		<br />香葱&蓝纹乳酪塔 <span>￥48.00/RMB</span>
+		<br />烤土豆，凉拌卷心菜，配沙拉 <span>￥38.00/RMB</span>
+			另配金枪鱼 <span>￥48.00/RMB</span>
+		<br />烤土豆，番茄焗豆，芝士，配沙拉 <span>￥38.00/RMB</span>
+		<br />培根，蘑菇，芝麻菜，口袋饼，配沙拉 <span>￥48.00/RMB</span>
 	</p>
 	  
+	<p><strong>乳酪塔</strong>
+		<br />培根菠菜乳酪塔 <span>￥48.00/RMB</span>
+		<br />蘑菇番茄乳酪塔 <span>￥38.00/RMB</span>
+	</p>
+	
+	<p><strong>附加菜</strong>
+		<br />水果沙拉配椰奶或酸奶 <span>￥28.00/RMB</span>
+	</p>
 	  
 	  
 </div>
