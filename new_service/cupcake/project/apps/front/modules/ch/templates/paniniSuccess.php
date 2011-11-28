f110edfbc9064d9cb58ecbf5dd4031ad0d2dfcaa
simple.web.service <simple.web.service@5809462c-694a-0410-bf31-61cd765fbef8>
2011-11-28 16:50:20 +0000

    According to CaoFeng's mail to update menu and product.
    Add backup.

--- new_service/cupcake/project/apps/front/modules/ch/templates/paniniSuccess.php
+++ new_service/cupcake/project/apps/front/modules/ch/templates/paniniSuccess.php
@@ -13,24 +13,24 @@
 	<dt><img src="/images_ch/Menu_.gif" alt="" /></dt>
 	<dd><a href="<?php echo url_for('ch/cupcakes') ?>"><img src="/images_ch/Menu_01.gif" alt="" border="0" /></a></dd>
 	<dd><img src="/images_ch/Menu_08_1.gif" alt="" width="146" height="23" /></dd>
-	
-	
+
+
 	</dl>
-	
-	
+
+
 	</div>
 	<div class="menuContentRight">
 	<div style="height:330px; overflow-y:scroll">
-	
-	
-	
+
+
+
 <?php if (1) : ?>
 
 
 	<?php
-	
+
 	echo	App_Menu::getLangMenuHtml('ch');
-	
+
 	?>
 
 <?php else : ?>
@@ -38,66 +38,65 @@
 
 	<p><strong>帕尼尼</strong>
 		<br />秘制番茄酱，鲜罗勒，大孔奶酪 <span>￥38.00/RMB</span>
-		<br />意大利腊肠，烤红柿子椒，紫菊苣，大孔奶酪 <span>￥48.00/RMB</span>
 		<br />烟熏鸡胸肉，炒蘑菇，大孔奶酪 <span>￥48.00/RMB</span>
 		<br />炒牛肉，红柿子椒，洋葱，大孔奶酪 <span>￥48.00/RMB</span>
 	</p>
-	  
-	
+
+
 	<p><strong>百吉饼</strong>
 		<br />烤培根，鸡蛋蛋黄酱，西红柿，黄油生菜 <span>￥38.00/RMB</span>
-		<br />火腿，菠萝，秘制青椒，罗马生菜，青柠蛋黄酱 <span>￥48.00/RMB</span>
 		<br />烟熏三文鱼，柠檬奶油乳酪，香葱，黄瓜，芝麻菜 <span>￥58.00/RMB</span>
 		<br />烤鸡腿肉，西红柿，奶油生菜 <span>￥48.00/RMB</span>
-		<br />烤鱼肉，泰式蛋黄酱，黄瓜，西红柿，芝麻菜 <span>￥58.00/RMB</span>
 	</p>
-	
-	
+
+
 	<p><strong>卷饼</strong>
-		<br />三巴虾，卜卜米，西洋菜，泰式汁 <span>￥48.00/RMB</span>
+		<br />火腿，海鲜酱 ，柿子椒，洋葱，黄瓜，罗马生菜 <span>￥48.00/RMB</span>
 		<br />烤鸭，海鲜酱，罗勒酱，黄瓜，香葱，虾片，黄油生菜 <span>￥48.00/RMB</span>
 		<br />金枪鱼沙拉，大孔奶酪，凯撒酱 <span>￥48.00/RMB</span>
 	</p>
-	
-	
+
+
 	<p><strong>法棍面包</strong>
-		<br />香烤牛肉，酸黄瓜，辣根酱，芝麻菜 <span>￥48.00/RMB</span>
 		<br />烟熏鸡胸肉，鳄梨果，蟹肉，鲜番茄，青柠蛋黄酱，罗马生菜 <span>￥48.00/RMB</span>
-		<br />意大利香肠，秘制紫椰菜，番茄酱 <span>￥48.00/RMB</span>
-		<br />水牛芝士，紫苏酱，鲜番茄，奶油生菜 <span>￥48.00/RMB</span>
 	</p>
-	
-	
+
+
 	<p><strong>沙拉</strong>
 		<br />金枪鱼沙拉 <span>￥48.00/RMB</span>
 		<br />烤菜花，烤紫菊苣，飞达芝士，培根，烤面包丁，鹌鹑蛋，红酒醋汁 <span>￥48.00/RMB</span>
-		<br />意大利面沙拉 <span>￥48.00/RMB</span>
+		<br />烤蘑菇培根沙拉 <span>￥48.00/RMB</span>
 	</p>
-	
+
 	<p><strong>汤</strong>
 		<br />南瓜汤 <span>￥28.00/RMB</span>
 		<br />芦笋汤 <span>￥28.00/RMB</span>
+		<br />奶油蘑菇汤 <span>￥28.00/RMB</span>
+		<br />奶油蘑菇汤 <span>￥28.00/RMB</span>
 	</p>
 
+	<!--
 	<p><strong>其他</strong>
 		<br />烤土豆，凉拌卷心菜，配沙拉 <span>￥38.00/RMB</span>
 			另配金枪鱼 <span>￥48.00/RMB</span>
 		<br />烤土豆，番茄焗豆，芝士，配沙拉 <span>￥38.00/RMB</span>
 		<br />培根，蘑菇，芝麻菜，口袋饼，配沙拉 <span>￥48.00/RMB</span>
 	</p>
-	  
+	-->
+
 	<p><strong>乳酪塔</strong>
 		<br />培根菠菜乳酪塔 <span>￥48.00/RMB</span>
 		<br />蘑菇番茄乳酪塔 <span>￥38.00/RMB</span>
 	</p>
-	
+
 	<p><strong>附加菜</strong>
-		<br />水果沙拉配椰奶或酸奶 <span>￥28.00/RMB</span>
+		<br />有机酸奶配水果莎莎 <span>￥39.00/RMB</span>
 	</p>
-	
+
+
 <?php endif ?>
 
 
-	  
+
 </div>
 	</div>
--- new_service/cupcake/project/lib/SofavDB_Model/sf_table_data_cart.class.php
+++ new_service/cupcake/project/lib/SofavDB_Model/sf_table_data_cart.class.php
@@ -20,9 +20,9 @@ class Table_data_cart extends SofavDB_Table {
 		$this->hasColumns($arrColumns);
 
 	}
-	
+
 	public static function calcCartSum($strCartID, $strField = 'cart_id', $strTable = 'Table_data_cart_detail') {
-		
+
 		$criteria	= new SofavDB_Criteria(sprintf('WHERE @where'));
 		$arrParam	= array(
 					'cart_id'	=> $strCartID,
@@ -36,39 +36,39 @@ class Table_data_cart extends SofavDB_Table {
 			$fltTotal		+= (float) $val['total'];
 
 		}
-		
+
 		$intTotal	= (int) $fltTotal;
-		
+
 		if ($intTotal > 0) {
-			
+
 		} else {
 			$intTotal	= 0;
 		}
-		
+
 		return	$intTotal;
-		
-		
+
+
 	}
-	
-	
+
+
 	public static function genDeliverTimes() {
-		
+
 		$arrHtml	= array();
-		
+
 		$intTime	= strtotime(date('Y-m-d') . ' 10:00:00');
-		
-		for ($i = 0; $i < 23; $i++) {
-		
+
+		for ($i = 0; $i < 21; $i++) {
+
 			$arrHtml[]	= sprintf('<option value="%s">%s</option>',
 						date('H:i', $intTime), date('H:ia', $intTime)
-					);	
-					
+					);
+
 			$intTime	+= 1800;
-			
+
 		}
-		
+
 		return	implode("\n", $arrHtml);
-		
+
 	}
 
 }
